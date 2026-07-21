246119aa755c240b15dae7f059da061c35570cdc
Liliana Beatriz Escobar <113596545+Liliana993@users.noreply.github.com>
2026-07-21 14:13:34 -0300

    Update mongoURI and jwtSecret defaults in config

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const config = {
   port: process.env.PORT || 3000,
-  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase',
-  jwtSecret: process.env.JWT_SECRET || 'mysecret',
+  mongoURI: process.env.MONGO_URI || 'mongodb://localhost:00000/mydatabase',
+  jwtSecret: process.env.JWT_SECRET || 'my_secret',
   nodeEnv: process.env.NODE_ENV || 'development',
 };
 
