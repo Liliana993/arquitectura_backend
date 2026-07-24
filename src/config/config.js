987b43d2b8bd03294e8efc7ac1e6f0531244cc71
Liliana993 <lili_esco@yahoo.com.ar>
2026-07-24 15:02:41 -0300

    correccion de errores

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const config = {
+export const config = {
   port: process.env.PORT || 3000,
   mongoURI: process.env.MONGO_URI || 'mongodb://localhost:00000/mydatabase',
   jwtSecret: process.env.JWT_SECRET || 'my_secret',
   nodeEnv: process.env.NODE_ENV || 'development',
 };
-
-export default config;
